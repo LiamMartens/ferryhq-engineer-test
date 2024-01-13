ebe0fc30dfe82e0ee9be0243926005408de2a275
Liam Martens <hi@liammartens.com>
2024-01-13 11:19:35 -0500

    feat: coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,4 +3,20 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    include: ["./src/utils/**/*.test.{ts,tsx}"],
+    coverage: {
+      provider: "v8",
+      include: [],
+      exclude: [
+        "**/*.{d.ts,config.ts,js,mjs,cjs}",
+        "**/styled-system/**",
+        "**/.storybook/**",
+        "**/src/app/**",
+        "**/src/types/**",
+        "**/src/ui/**",
+      ],
+      reporter: ["text"],
+    },
+  },
 });
