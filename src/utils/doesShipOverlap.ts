3f7f1c348fbf008653a62f42c2fb597849cfa113
Liam Martens <hi@liammartens.com>
2024-01-14 18:15:05 -0500

    fix: dont allow collisions

--- src/utils/doesShipOverlap.ts
+++ src/utils/doesShipOverlap.ts
@@ -2,9 +2,14 @@ import { GridCoordinate } from "@/types";
 
 export function doesShipOverlap(
   shipA: GridCoordinate[],
-  shipB: GridCoordinate[]
+  shipB: GridCoordinate[],
+  minDistance = 1
 ) {
   return shipA.some(([column, row]) =>
-    shipB.some((bcoord) => bcoord[0] === column && bcoord[1] === row)
+    shipB.some((bcoord) => {
+      const coldiff = Math.abs(bcoord[0] - column);
+      const rowdiff = Math.abs(bcoord[1] - row);
+      return coldiff + rowdiff < minDistance;
+    })
   );
 }
